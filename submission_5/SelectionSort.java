77938569f160fc3268d787480e996e2c5182a920
wethulisethnara <wethulisethnara@gmail.com>
2026-06-20 21:58:15 +0530

    fifth commit

---
+++ submission_5/SelectionSort.java
@@ -0,0 +1,16 @@
+package submission_5;
+
+public class SelectionSort {
+    public void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) minIdx = j;
+            }
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
